ec9ed2ffd161e5714afd4213f131454d71570440
jrgiant <jrgiant_git@outlook.com>
2017-03-25 21:57:38 -0400

    ES6 version of the javascript
    FInally fully functional

--- JS/js.js
+++ JS/js.js
@@ -1,11 +1,22 @@
 const FIELD_PREFIX ='#id_profile_field_';
 const FIELD_WRAPPER = '#fitem_id_profile_field_';
+/** Class add the hide or Show ability to the children */
 class hideShow{
+  /**
+   * create the vairbles
+   * @param {sting} par - Name of the Parent Field
+   * @param {array} pv - Values from the parent field
+   * @param {array} ct - Children to show
+   */
   constructor(par, pv, ct){
     this.parent = FIELD_PREFIX + par;
     this.parentValues = pv;
     this.childrenToShow = ct;
   }
+  /**
+   *  @method  exec
+   * @desc Preforms the actions required to hide or show the children fields
+   */
   exec(){
     var currentValue = document.querySelector(this.parent).value;
     var indexes = getAllIndexes(this.parentValues, currentValue);
@@ -17,6 +28,42 @@ class hideShow{
     indexes.forEach(i => {
       document.querySelector(FIELD_WRAPPER + this.childrenToShow[i]).setAttribute('aria-hidden','false');
     });
+  }
+}
+/** class updates the values in the child field */
+class update{
+  /**
+   *  create the varibles
+   * @param {string} pa - Name of Parent field
+   * @param {string} cu - Name of current Field
+   * @param {array} pv - Parent Values (a.k.a trigger values)
+   * @param {array} cv - children Values (a.k.a update values)
+   * @param {string=} sv - Phrase to show as the first option with value of -1
+   */
+  constructor(pa, cu, pv, cv, sv){
+    this.parent = FIELD_PREFIX + pa;
+    this.currentField = FIELD_PREFIX + cu
+    this.parentValues = pv;
+    this.childrenValues = cv;
+    this.selectPhrase = sv || 'Select';
+  }
+  /**
+   *  @method  exec
+   * @desc Preforms the actions required to hide or show the children fields
+   */
+  exec(){
+    var currentValue = document.querySelector(this.parent).value;
+    if (currentValue === "-1"){ return false;}
+    var indexes = getAllIndexes(this.parentValues, currentValue);
+    var valuesToShow = indexes.map(i => this.childrenValues[i]);
+    var thisSelect = document.querySelector(this.currentField);
+    thisSelect.innerHTML = `<option value="-1">${this.selectPhrase}</option>`;
+    valuesToShow.forEach(v => {
+      var o = document.createElement('option');
+      o.text = v;
+      o.value = v;
+      thisSelect.add(o);})
+
   }
 }
 
