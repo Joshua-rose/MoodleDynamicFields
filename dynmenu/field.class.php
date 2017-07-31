93f4e97aa6ff95855c016c18f66de8754e427c67
JrGiant <JrGiant_Git@outlook.com>
2017-07-31 18:11:46 -0400

    Added Functionality for Hide/show fields

--- dynmenu/field.class.php
+++ dynmenu/field.class.php
@@ -47,6 +47,10 @@
     /** @var array $triggerValues */
     public $triggerValues;
 
+    /** @var string $field_wrapper */
+    $field_wrapper ='#fitem_id_profile_field_';
+    /** @var string $field_prefix */
+    $field_prefix = '#id_profile_field_';
     /**
      * Constructor method.
      *
@@ -129,15 +133,13 @@
    if (!isset($CFG->additionalhtmlfooter)) {
      $CFG->additionalhtmlfooter = '';
    }
- $field_wrapper ='#fitem_id_profile_field_';
- $field_prefix = '#id_profile_field_';
- $sStart = '<script type="text/javascript">';
+   $sStart = '<script type="text/javascript">';
 $sEnd = '</script>';
 $CFG->additionalhtmlfooter .= $sStart.$this->dynmenu_add_javascript($fieldType).$sEnd;
 $jsMin = $sStart;
- $jsMin .= "var _createClass=function(){function a(b,c){for(var e,d=0;d<c.length;d++)e=c[d],e.enumerable=e.enumerable||!1,e.configurable=!0,'value'in e&&(e.writable=!0),Object.defineProperty(b,e.key,e)}return function(b,c,d){return c&&a(b.prototype,c),d&&a(b,d),b}}();function _classCallCheck(a,b){if(!(a instanceof b))throw new TypeError('Cannot call a class as a function')}var ".$field_prefix."='#id_profile_field_',".$field_wrapper."='#fitem_id_profile_field_',hideSho=function(){function";
- $jsMin .= "a(b,c,d){_classCallCheck(this,a),this.parent=".$field_prefix."+b,this.parentValues=c,this.childrenToShow=d}return _createClass(a,[{key:'exec',value:function exec(){var d=this,b=document.querySelector(this.parent).value,c=getAllIndexes(this.parentValues,b);this.childrenToShow.forEach(function(e){document.querySelector(".$field_wrapper."+e).setAttribute('aria-hidden','true'),document.querySelector(".$field_prefix."+e).value='-1';var f=new";
- $jsMin .= "Event('change');document.querySelector(".$field_prefix."+e).dispatchEvent(f)}),c.forEach(function(e){document.querySelector(".$field_wrapper."+d.childrenToShow[e]).setAttribute('aria-hidden','false')})}}]),a}(),update=function(){function a(b,c,d,e,f){_classCallCheck(this,a),this.parent=".$field_prefix."+b,this.currentField=".$field_prefix."+c,this.parentValues=d,this.childrenValues=e,this.selectPhrase=f||'Select'}return _createClass(a,[{key:'exec',value:function exec(){var";
+ $jsMin .= "var _createClass=function(){function a(b,c){for(var e,d=0;d<c.length;d++)e=c[d],e.enumerable=e.enumerable||!1,e.configurable=!0,'value'in e&&(e.writable=!0),Object.defineProperty(b,e.key,e)}return function(b,c,d){return c&&a(b.prototype,c),d&&a(b,d),b}}();function _classCallCheck(a,b){if(!(a instanceof b))throw new TypeError('Cannot call a class as a function')}var ".$GLOBALS[field_prefix]."='#id_profile_field_',".$GLOBALS[field_wrapper]."='#fitem_id_profile_field_',hideSho=function(){function";
+ $jsMin .= "a(b,c,d){_classCallCheck(this,a),this.parent=".$GLOBALS[field_prefix]."+b,this.parentValues=c,this.childrenToShow=d}return _createClass(a,[{key:'exec',value:function exec(){var d=this,b=document.querySelector(this.parent).value,c=getAllIndexes(this.parentValues,b);this.childrenToShow.forEach(function(e){document.querySelector(".$GLOBALS[field_wrapper]."+e).setAttribute('aria-hidden','true'),document.querySelector(".$GLOBALS[field_prefix]."+e).value='-1';var f=new";
+ $jsMin .= "Event('change');document.querySelector(".$GLOBALS[field_prefix]."+e).dispatchEvent(f)}),c.forEach(function(e){document.querySelector(".$GLOBALS[field_wrapper]."+d.childrenToShow[e]).setAttribute('aria-hidden','false')})}}]),a}(),update=function(){function a(b,c,d,e,f){_classCallCheck(this,a),this.parent=".$GLOBALS[field_prefix]."+b,this.currentField=".$GLOBALS[field_prefix]."+c,this.parentValues=d,this.childrenValues=e,this.selectPhrase=f||'Select'}return _createClass(a,[{key:'exec',value:function exec(){var";
  $jsMin .= "f=this,b=document.querySelector(this.parent).value;if('-1'===b)return!1;var c=getAllIndexes(this.parentValues,b),d=c.map(function(g){return f.childrenValues[g]}),e=document.querySelector(this.currentField);e.innerHTML='<option value=\"-1\">'+this.selectPhrase+'</option>',d.forEach(function(g){var h=document.createElement('option');h.text=g,h.value=g,e.add(h)})}}]),a}();function getAllIndexes(a,b){var d,c=[];for(d=0;d<a.length;d++)a[d]===b&&c.push(d);return c}";
 $jsMin .= $sEnd;
 
@@ -147,19 +149,21 @@ $jsMin .= $sEnd;
  }
 public function add_JS_arrays_and_exec_call_to_class()
 {
+  $js = '';
   switch ($this->fieldType) {
     case 0:
     //this is a parent field
-    // TODO: get children and add them to the $CFG->parentChildFields
+    // TODO: Show Hide/Show Children
       # code...
       break;
     case 1:
     //this is a showhide field
-    // TODO: if this field has children add them to the $CFG->parentChildFields
+    //code should be handeled by parent
       # code...
       break;
     case 2:
     //this is a update field
+
       # code...
       break;
     case 3:
@@ -173,7 +177,55 @@ public function add_JS_arrays_and_exec_call_to_class()
       # code...
       break;
   }
+  add_children_to_parentchildfields();
+  return getChildTypeJS()+$js;
 }
+/**
+ * checks the child field types to add the proper JavaScript
+ */
+ public function getChildTypeJS()
+ {
+   $js = '';
+   switch ($this->childType) {
+     case 1:
+     case 3:
+     $isFirst = true;
+     $js .= "var paHS_{$this->field->shortname} = [";
+      foreach ($this->options as $key => $option) {
+        if(!$isFirst){
+        $js .= ',"'.$option.'"';
+        }
+        else {
+        $js .= '"'.$option.'"';
+        $isFirst = false;
+        }
+      }
+      $js .= "];\n";
+      $isFirst = true;
+      $js .= "var caHS_{$this->field->shortname} = [";
+       foreach ($this->childrenNames as $key => $child) {
+         if(!$isFirst){
+         $js .= ',"'.$child.'"';
+         }
+         else {
+         $js .= '"'.$child.'"';
+         $isFirst = false;
+         }
+       }
+       $js .= "];\n";
+       $js .= "var show{$this->field->shortname}Children = new hideShow('{$this->field->shortname}',psHS_{$this->field->shortname}, csHS_{$this->field->shortname})\n";
+       $js .= "show{$this->field->shortname}Children.exec()\n";
+       $js .= "document.querySelector({$GLOBALS[field_prefix]} + '{$this->field->shortname}').addEventListener('change', function () {show{$this->field->shortname}Children.exec();});";
+       break;
+     case 2:
+
+       break;
+     default:
+       # code...
+       break;
+   }
+   return $js;
+ }
 /**
  * Add the children elements to the $CFG->parentChildFields array
  */
