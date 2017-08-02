cbbe7d520491205a8efbb602a7179f1c3194be60
jrgiant <jrgiant_git@outlook.com>
2017-08-02 11:32:28 -0400

    unminimized min.js for debugging

--- dynmenu/field.class.php
+++ dynmenu/field.class.php
@@ -143,7 +143,128 @@ if (!isset($CFG->additionalhtmlfooter)) {
   $CFG->additionalhtmlfooter = "{$sStart}console.log('begin DynMenuLogs');{$sEnd}";
 }
 $jsMin = $sStart;
- $jsMin .= "'use strict';var _createClass=function(){function a(b,c){for(var e,d=0;d<c.length;d++)e=c[d],e.enumerable=e.enumerable||!1,e.configurable=!0,'value'in e&&(e.writable=!0),Object.defineProperty(b,e.key,e)}return function(b,c,d){return c&&a(b.prototype,c),d&&a(b,d),b}}();function _classCallCheck(a,b){if(!(a instanceof b))throw new TypeError('Cannot call a class as a function')}var FIELD_PREFIX='".$field_prefix."',FIELD_WRAPPER='".$field_wrapper."',hideShow=function(){function a(b,c,d){_classCallCheck(this,a),this.parent=FIELD_PREFIX+b,this.parentValues=c,this.childrenToShow=d}return _createClass(a,[{key:'exec',value:function exec(){var d=this,b=document.querySelector(this.parent).value,c=getAllIndexes(this.parentValues,b);this.childrenToShow.forEach(function(e){document.querySelector(FIELD_WRAPPER+e).setAttribute('aria-hidden','true'),document.querySelector(FIELD_PREFIX+e).value='-1';var f=new Event('change');document.querySelector(FIELD_PREFIX+e).dispatchEvent(f)}),c.forEach(function(e){document.querySelector(FIELD_WRAPPER+d.childrenToShow[e]).setAttribute('aria-hidden','false')})}}]),a}(),update=function(){function a(b,c,d,e,f){_classCallCheck(this,a),this.parent=FIELD_PREFIX+b,this.currentField=FIELD_PREFIX+c,this.parentValues=d,this.childrenValues=e,this.selectPhrase=f||'Select'}return _createClass(a,[{key:'exec',value:function exec(){var f=this,b=document.querySelector(this.parent).value;if('-1'===b)return!1;var c=getAllIndexes(this.parentValues,b),d=c.map(function(g){return f.childrenValues[g]}),e=document.querySelector(this.currentField);e.innerHTML='<option value=\"-1\">'+this.selectPhrase+'</option>',d.forEach(function(g){var h=document.createElement('option');h.text=g,h.value=g,e.add(h)})}}]),a}();function getAllIndexes(a,b){var d,c=[];for(d=0;d<a.length;d++)a[d]===b&&c.push(d);return c}";
+ $jsMin .= <<<minjs
+ 'use strict';
+
+ var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+ var FIELD_PREFIX = '#id_profile_field_';
+ var FIELD_WRAPPER = '#fitem_id_profile_field_';
+ /** Class add the hide or Show ability to the children */
+
+ var hideShow = function () {
+   /**
+    * create the vairbles
+    * @param {sting} par - Name of the Parent Field
+    * @param {array} pv - Values from the parent field
+    * @param {array} ct - Children to show
+    */
+   function hideShow(par, pv, ct) {
+     _classCallCheck(this, hideShow);
+
+     this.parent = FIELD_PREFIX + par;
+     this.parentValues = pv;
+     this.childrenToShow = ct;
+   }
+   /**
+    *  @method  exec
+    * @desc Preforms the actions required to hide or show the children fields
+    */
+
+
+   _createClass(hideShow, [{
+     key: 'exec',
+     value: function exec() {
+       var _this = this;
+
+       var currentValue = document.querySelector(this.parent).value;
+       var indexes = getAllIndexes(this.parentValues, currentValue);
+       this.childrenToShow.forEach(function (child) {
+         document.querySelector(FIELD_WRAPPER + child).setAttribute('aria-hidden', 'true');
+         document.querySelector(FIELD_PREFIX + child).value = "-1";
+         var event = new Event('change');
+         document.querySelector(FIELD_PREFIX + child).dispatchEvent(event);
+       });
+       indexes.forEach(function (i) {
+         document.querySelector(FIELD_WRAPPER + _this.childrenToShow[i]).setAttribute('aria-hidden', 'false');
+       });
+     }
+   }]);
+
+   return hideShow;
+ }();
+ /** class updates the values in the child field */
+
+
+ var update = function () {
+   /**
+    *  create the varibles
+    * @param {string} pa - Name of Parent field
+    * @param {string} cu - Name of current Field
+    * @param {array} pv - Parent Values (a.k.a trigger values)
+    * @param {array} cv - children Values (a.k.a update values)
+    * @param {string=} sv - Phrase to show as the first option with value of -1
+    */
+   function update(pa, cu, pv, cv, sv) {
+     _classCallCheck(this, update);
+
+     this.parent = FIELD_PREFIX + pa;
+     this.currentField = FIELD_PREFIX + cu;
+     this.parentValues = pv;
+     this.childrenValues = cv;
+     this.selectPhrase = sv || 'Select';
+   }
+   /**
+    *  @method  exec
+    * @desc Preforms the actions required to hide or show the children fields
+    */
+
+
+   _createClass(update, [{
+     key: 'exec',
+     value: function exec() {
+       var _this2 = this;
+
+       var currentValue = document.querySelector(this.parent).value;
+       if (currentValue === "-1") {
+         return false;
+       }
+       var indexes = getAllIndexes(this.parentValues, currentValue);
+       var valuesToShow = indexes.map(function (i) {
+         return _this2.childrenValues[i];
+       });
+       var thisSelect = document.querySelector(this.currentField);
+       thisSelect.innerHTML = '<option value="-1">' + this.selectPhrase + '</option>';
+       valuesToShow.forEach(function (v) {
+         var o = document.createElement('option');
+         o.text = v;
+         o.value = v;
+         thisSelect.add(o);
+       });
+     }
+   }]);
+
+   return update;
+ }();
+
+ /**
+ * get all of the indexes for the values within an array
+ * @param {array} arr - array to check
+ * @param {*} value - to be found in array
+ * @see @link{http://stackoverflow.com/questions/20798477}
+ */
+
+ 
+ function getAllIndexes(arr, val) {
+   var indexes = [],
+       i;
+   for (i = 0; i < arr.length; i++) {
+     if (arr[i] === val) indexes.push(i);
+   }return indexes;
+ }
+minjs
 $jsMin .= $sEnd;
 
  if(strpos($CFG->additionalhtmlfooter, $jsMin) === false){
