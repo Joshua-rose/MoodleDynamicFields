f44528c6a5e4b24dd0364da1f39b6f13eb3439db
JrGiant <jrgiant_git@outlook.com>
2017-08-06 13:16:47 -0400

    Fixed quotation issue with UpdateChild

--- dynmenu/field.class.php
+++ dynmenu/field.class.php
@@ -341,7 +341,7 @@ private function updateValueJS()
     }
     if (isset($parentField)){
       $js .= "var update{$this->field->shortname}_{$parentField} = new update('{$parentField}', '{$this->field->shortname}',tvUP_{$this->field->shortname}, caUP_{$this->field->shortname})\n";
-      $js .= "document.querySelector('{$field_prefix}' + {$parentField}).addEventListener('change', function () {update{$this->field->shortname}_{$parentField}.exec();});";
+      $js .= "document.querySelector('{$field_prefix}' + '{$parentField}').addEventListener('change', function () {update{$this->field->shortname}_{$parentField}.exec();});";
     }
     return $js;
 }
